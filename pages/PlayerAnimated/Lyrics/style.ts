4be49d1eb1cc6f8524dba71b00b173d7e1220cda
Miguel Penha <miguel.penha1@gmail.com>
2022-12-04 21:25:45 -0300

    Update style.ts

--- pages/PlayerAnimated/Lyrics/style.ts
+++ pages/PlayerAnimated/Lyrics/style.ts
@@ -5,8 +5,8 @@ import { RFPercentage } from 'react-native-responsive-fontsize'
 
 export const Container = styled(Animated.createAnimatedComponent(TouchableOpacity))`
     width: 90%;
-    height: 16%;
-    margin-top: 10%;
+    height: 17%;
+    margin-top: 8%;
     overflow: hidden;
     margin-bottom: 5%;
     align-self: center;
