37f7b04d0df73f1216b1751b0b3ea0d0bb3ea928
Miguel Penha <miguel.penha1@gmail.com>
2022-12-17 00:16:35 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,31 +1,31 @@
-import * as SplashScreen from 'expo-splash-screen'
 import React, { useState, useEffect } from 'react'
 import updateApp from './utils/updateApp'
+import AppLoading from 'expo-app-loading'
 import { green } from './utils/colorsLogs'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 import Routes from './routes'
 import 'react-native-gesture-handler'
 
-SplashScreen.preventAutoHideAsync()
-
 function App() {
   const [isReady, setReady] = useState(false)
 
   useEffect(() => {
-    async function prepare() {
-      await updateApp()
+    updateApp().then()
 
-      setReady(true)
-
-      await SplashScreen.hideAsync()
-    }
-
-    prepare().then()
+    setReady(true)
   }, [])
 
   if (!isReady) {
-    return null
+    return <AppLoading/>
+  } else {
+    console.log(green('>> App Started'))
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Routes/>
+      </ThemeProvider>
+    )
   }
 
   console.log(green('>> App Started'))
