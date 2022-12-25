060b9ed13a15021e1939b614fc103d881600cc01
Miguel Penha <miguel.penha1@gmail.com>
2022-12-24 23:55:08 -0300

    Update index.tsx

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,13 +1,13 @@
 import ContainerPd from '../../components/ContainerPd'
 import { Title } from './style'
-import { ZoomInDown } from 'react-native-reanimated'
+import { ZoomInUp } from 'react-native-reanimated'
 import OptionsList from './OptionsList'
 import options from './options'
 
 function Home() {
   return (
     <ContainerPd>
-      <Title entering={ZoomInDown}>Testando Animações</Title>
+      <Title entering={ZoomInUp}>Testando Animações</Title>
       <OptionsList options={options}/>
     </ContainerPd>
   )
