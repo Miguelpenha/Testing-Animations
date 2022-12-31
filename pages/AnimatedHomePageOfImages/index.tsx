bcd0378816c49db4c10e498639dc42c93ac0eb4b
Miguel Penha <miguel.penha1@gmail.com>
2022-12-30 22:18:35 -0300

    Update index.tsx

--- pages/AnimatedHomePageOfImages/index.tsx
+++ pages/AnimatedHomePageOfImages/index.tsx
@@ -3,6 +3,7 @@ import ContainerPd from '../../components/ContainerPd'
 import HeaderBack from '../../components/HeaderBack'
 import { Images, ContainerColumns, ColumnImages} from './style'
 import ImageAnimated from './ImageAnimated'
+import Loading from '../../components/Loading'
 
 function AnimatedHomePageOfImages() {
     const { imagesRow1, imagesRow2 } = useImages()
@@ -10,16 +11,20 @@ function AnimatedHomePageOfImages() {
     return (
         <ContainerPd>
             <HeaderBack>Página inicial animada de imagens</HeaderBack>
-            <Images contentContainerStyle={{paddingBottom: '40%'}}>
-                <ContainerColumns>
-                    <ColumnImages>
-                        {imagesRow1.map((image, index) => <ImageAnimated key={index} image={image} index={index}/>)}
-                    </ColumnImages>
-                    <ColumnImages>
-                        {imagesRow2.map((image, index) => <ImageAnimated key={index} image={image} index={index}/>)}
-                    </ColumnImages>
-                </ContainerColumns>
-            </Images>
+            {(imagesRow1.length > 0 || imagesRow2.length > 0) ? (
+                <Images contentContainerStyle={{paddingBottom: '40%'}}>
+                    <ContainerColumns>
+                        <ColumnImages>
+                            {imagesRow1.map((image, index) => <ImageAnimated key={index} image={image} index={index}/>)}
+                        </ColumnImages>
+                        <ColumnImages>
+                            {imagesRow2.map((image, index) => <ImageAnimated key={index} image={image} index={index}/>)}
+                        </ColumnImages>
+                    </ContainerColumns>
+                </Images>
+            ) : (
+                <Loading/>
+            )}
         </ContainerPd>
     )
 }
