c0b90872fea99150752a4681d9cdf5edc6d348b1
Miguel Penha <miguel.penha1@gmail.com>
2023-11-17 22:13:28 -0300

    Teste

--- utils/updateApp.ts
+++ utils/updateApp.ts
@@ -1,27 +1,22 @@
-import Constants from 'expo-constants'
 import * as Updates from 'expo-updates'
 import { green } from './colorsLogs'
 
-async function updateApp(response?: boolean): Promise<void | boolean> {
-  if (Constants.expoConfig?.extra?.NODE_ENV === 'production') {
+async function updateApp() {
+  if (process.env.NODE_ENV === 'production') {
     const { isAvailable } = await Updates.checkForUpdateAsync()
-
+    
     if (isAvailable) {
-      if (Updates.releaseChannel === 'production' || Updates.releaseChannel === 'main') {
         console.log(green('>> Update Available'))
 
         await Updates.fetchUpdateAsync()
         await Updates.reloadAsync()
-        
-        if (response) {
-          return true
-        }
-      }
+
+        return true
     } else {
-      if (response) {
         return false
-      }
     }
+  } else {
+      return false
   }
 }
 
