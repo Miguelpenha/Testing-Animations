a19fea7b2163c5e94bca698919a8a96eaa998374
Miguel Penha <miguel.penha1@gmail.com>
2023-11-18 03:23:53 -0300

    Update images.ts

--- pages/PlayerAnimated/images.ts
+++ pages/PlayerAnimated/images.ts
@@ -1,19 +1,15 @@
 import { IImage } from './type'
 import uuid from 'react-native-uuid'
 
-const urlImage = 'https://e-cdns-images.dzcdn.net/images/cover/b69d3bcbd130ad4cc9259de543889e30/380x380-000000-80-0-0.jpg'
-
-const images: IImage[] = []
-
-for (let count = 0;count < 5;count++) {
-    images.push({
-        url: urlImage,
+let images: IImage[] = [
+    {
         artist: 'Drake',
         duration: '3:18',
         album: 'Scorpion',
         language: 'English',
+        music: "God's Plan",
         id: uuid.v4().toString(),
-        music: `God's Plan ${count+1}`,
+        url: 'https://i.scdn.co/image/ab67616d0000b273f907de96b9a4fbc04accc0d5',
         lyrics: `Yeah, they wishin' and wishin' and wishin' and wishin'
 They wishin' on me, yuh
 I been movin' calm, don't start no trouble with me
@@ -67,7 +63,315 @@ It's a lot of bad things
 That they wishin' and wishin' and wishin' and wishin'
 They wishin' on me
 Yeah`
-    })
-}
+    },
+    {
+        duration: '3:45',
+        language: 'English',
+        album: 'This Is America',
+        music: 'This Is America',
+        id: uuid.v4().toString(),
+        artist: 'Childish Gambino',
+        url: 'https://i.scdn.co/image/ab67616d0000b2733504664c17b7b99a3cb7d116',
+        lyrics: `Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, go, go away
+Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, go, go away
+Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, go, go away
+Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, go, go away
+We just wanna party
+Party just for you
+We just want the money
+Money just for you
+I know you wanna party (yeah)
+Party just for free
+Girl, you got me dancin' (girl, you got me dancin')
+Dance and shake the frame
+We just wanna party (yeah)
+Party just for you (yeah)
+We just want the money (yeah)
+Money just for you (ooh)
+I know you wanna party (yeah)
+Party just for free (yeah)
+Girl, you got me dancin' (girl, you got me dancin')
+Dance and shake the frame (ooh)
+This is America
+Don't catch you slippin' now
+Don't catch you slippin' now
+Look what I'm whippin' now
+This is America (woo)
+Don't catch you slippin' now
+Don't catch you slippin' now
+Look what I'm whippin' now
+This is America (skrrt, skrrt, woo)
+Don't catch you slippin' now (ayy)
+Look at how I'm livin' now
+Police be trippin' now (woo)
+Yeah, this is America (woo, ayy)
+Guns in my area (word, my area)
+I got the strap (ayy, ayy)
+I gotta carry 'em
+Yeah, yeah, I'ma go into this (ugh)
+Yeah, yeah, this is guerilla, woo
+Yeah, yeah, I'ma go get the bag
+Yeah, yeah, or I'ma get the pad
+Yeah, yeah, I'm so cold like yeah (yeah)
+I'm so dope like yeah (woo)
+We gon' blow like yeah (straight up, uh)
+Ooh-ooh-ooh-ooh-ooh, tell somebody
+You go tell somebody
+Grandma told me
+Get your money, black man (get your money)
+Get your money, black man (get your money)
+Get your money, black man (get your, black man)
+Get your money, black man (get your, black man)
+Black man
+This is America (woo, ayy)
+Don't catch you slippin' now (woo, woo, don't catch you slippin', now)
+Don't catch you slippin' now (ayy, woah)
+Look what I'm whippin' now (Slime)
+This is America (yeah, yeah)
+Don't catch you slippin' now (woah, ayy)
+Don't catch you slippin' now (ayy, woo)
+Look what I'm whippin' now (ayy)
+Look how I'm geekin' out (hey)
+I'm so fitted (I'm so fitted, woo)
+I'm on Gucci (I'm on Gucci)
+I'm so pretty (yeah, yeah)
+I'm gon' get it (ayy, I'm gon' get it)
+Watch me move (blaow)
+This a celly (ha)
+That's a tool (yeah)
+On my Kodak (woo, Black)
+Ooh, know that (yeah, know that, hold on)
+Get it (get it, get it)
+Ooh, work it (21)
+Hunnid bands, hunnid bands, hunnid bands (hunnid bands)
+Contraband, contraband, contraband (contraband)
+I got the plug on Oaxaca (woah)
+They gonna find you like blocka (blaow)
+Ooh-ooh-ooh-ooh-ooh, tell somebody
+(America, I just checked my following list and)
+You go tell somebody
+(You mothafuckas owe me)
+Grandma told me
+Get your money, black man (black man)
+Get your money, black man (black man)
+Get your money, black man (black man)
+Get your money, black man (black man)
+Black man (one, two, three, get down)
+Ooh-ooh-ooh-ooh-ooh, tell somebody
+You go tell somebody
+Grandma told me, "Get your money, " black man
+Get your money, black man (black man)
+Get your money, black man (black man)
+Get your money, black man (black man)
+Black man
+You just a black man in this world
+You just a barcode, ayy
+You just a black man in this world
+Drivin' expensive foreigns, ayy
+You just a big dawg, yeah
+I kenneled him in the backyard
+No, probably ain't life to a dog
+For a big dog`
+    },
+    {
+        duration: '3:57',
+        album: "It's Time",
+        language: 'English',
+        music: 'Felling Good',
+        id: uuid.v4().toString(),
+        artist: 'Nina Simone/Michael Bublé',
+        url: 'https://i.scdn.co/image/ab67616d0000b273c6ad2b2b62b581a23a7c1759',
+        lyrics: `Birds flying high
+You know how I feel
+Sun in the sky
+You know how I feel
+Breeze driftin' on by
+You know how I feel
+It's a new dawn
+It's a new day
+It's a new life
+For me
+And I'm feeling good
+I'm feeling good
+Fish in the sea
+You know how I feel
+River running free
+You know how I feel
+Blossom on a tree
+You know how I feel
+It's a new dawn
+It's a new day
+It's a new life
+For me
+And I'm feeling good
+Dragonfly out in the sun, you know what I mean, don't you know
+Butterflies all havin' fun, you know what I mean
+Sleep in peace when day is done, that's what I mean
+And this old world is a new world
+And a bold world
+For me
+For me
+Stars when you shine
+You know how I feel
+Scent of the pine
+You know how I feel
+Oh, freedom is mine
+And I know how I feel
+It's a new dawn
+It's a new day
+It's a new life
+It's a new dawn
+It's a new day
+It's a new life
+It's a new dawn
+It's a new day
+It's a new life
+It's a new life
+For me
+And I'm feeling good
+I'm feeling good
+I feel so good
+I feel so good`
+    },
+    {
+        duration: '3:15',
+        artist: 'Stromae',
+        album: 'Multitude',
+        language: 'French',
+        music: 'Fils de joie',
+        id: uuid.v4().toString(),
+        url: 'https://i.scdn.co/image/ab67616d0000b27331d6b27ffaa0b2f89234698a',
+        lyrics: `Être seul c'est difficile
+Et là, ça fait des années
+Et de juger c'est facile
+Surtout quand on n'y a pas goûté
+Le plus dur, ben, c'était la première fois
+Puis le plus dur, c'est de savoir quand s'ra la dernière fois
+C'est vrai, j'suis pas contre un peu d'tendresse de temps en temps
+Et puis cette fois-ci, ben, j'pourrais l'faire en l'insultant
+Oui tout est négociable dans la vie, moyennant paiement
+En plus j'suis sûrement son meilleur client
+Mais OH!
+Laissez donc ma maman
+Oui je sais. C'est vrai qu'elle n'est pas parfaite
+C'est un héros
+Et ce sera toujours fièrement que j'en parlerai
+Que j'en parlerai
+J'suis un fils de pute, comme ils disent
+Après tout c'qu'elle a fait pour eux
+Pardonne leur bêtise
+Ô chère mère!
+Ils te déshumanisent
+C'est plus facile
+Les mêmes te courtisent
+Et tout l'monde ferme les yeux
+Pourquoi tout le monde me déteste?
+Alors qu'c'est moi qui les nourrit
+Leurs vies seraient bien plus modestes
+Sans moi, elles s'raient pourries
+Le lit et la sécurité
+Ont un prix Madame
+Ben oui dans la vie tout se paie
+On n'te l'avait donc jamais appris
+On m'accuse de faire de la traite d'êtres humains
+Mais 50, 40, 30 ou 20%, c'est déjà bien
+Faudrait pas qu'elles se prennent un peu trop pour des mannequins
+Mesdames, ou devrais-je dire putains!
+Mais OH!
+Laissez donc ma maman
+Oui, je sais. C'est vrai qu'elle n'est pas parfaite
+C'est un héros
+Et ce s'ra toujours fièrement que j'en parlerai
+Que j'en parlerai
+J'suis un fils de pute, comme ils disent
+Après tout c'qu'elle a fait pour eux
+Pardonne leur bêtise
+Ô chère mère!
+Ils te déshumanisent
+C'est plus facile
+Les mêmes te courtisent
+Et tout l'monde ferme les yeux
+Je sais qu'c'est ton boulot
+Mais faut bien qu'j'fasse le mien, non?
+Entre l'tien et le mien
+La différence, c'est que moi je paie des impôts
+Allez, circulez, Madame
+Reprends tes papiers et c'qu'il te reste de dignité
+Pauvre femme, pffff
+Trouve-toi un vrai métier!
+Mais OH!
+Laissez donc ma maman
+Oui je sais. C'est vrai qu'elle n'est pas parfaite
+C'est un héros
+Et ce sera toujours fièrement que j'en parlerai
+Que j'en parlerai
+J'suis un fils de pute, comme ils disent
+Après tout c'qu'elle a fait pour eux
+Pardonne leur bêtise
+Ô chère mère!
+Ils te déshumanisent
+C'est plus facile
+Les mêmes te courtisent
+Et tout l'monde ferme les yeux`
+    },
+    {
+        duration: '1:59',
+        language: 'English',
+        artist: 'Ray Charles',
+        id: uuid.v4().toString(),
+        music: 'Hit the Road Jack',
+        album: "Unforgettable 60's Hits (50 Hits Remastered)",
+        url: 'https://i.scdn.co/image/ab67616d0000b273afb055fcddab0300b316b54a',
+        lyrics: `(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+What you say?
+(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+Woah, woman, oh, woman, don't treat me so mean
+You're the meanest old woman that I've ever seen
+I guess if you said so
+I'd have to pack my things and go (that's right)
+(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+What you say?
+(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+Now, baby, listen, baby, don't ya treat me this way
+'Cause I'll be back on my feet some day
+(Don't care if you do, 'cause it's understood)
+(You ain't got no money, you just ain't no good)
+Well, I guess if you say so
+I'd have to pack my things and go (that's right)
+(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+What you say?
+(Hit the road, Jack, and don't you come back)
+(No more, no more, no more, no more)
+(Hit the road, Jack, and don't you come back no more)
+Well
+(Don't you come back no more)
+Uh, what you say?
+(Don't you come back no more)
+I didn't understand you!
+(Don't you come back no more)
+You can't mean that!
+(Don't you come back no more)
+Oh, no, baby, please!
+(Don't you come back no more)
+What you tryin' to do to me?
+(Don't you come back no more)
+Oh, don't treat me like that!
+(Don't you come back no more)`
+    }
+]
 
 export default images
