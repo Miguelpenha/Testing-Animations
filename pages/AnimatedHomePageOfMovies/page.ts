d17762c7990c56ffaaaddaaf1fe9f2dcfc6983da
Miguel Penha <miguel.penha1@gmail.com>
2023-12-16 00:59:58 -0300

    Adicionando Flash List nas descrições

--- pages/AnimatedHomePageOfMovies/page.ts
+++ pages/AnimatedHomePageOfMovies/page.ts
@@ -3,7 +3,7 @@ import { IPage } from '../../types'
 const page: IPage = {
     apis: ['The MovieDB'],
     title: 'Página inicial animada de filmes',
-    libs: ['React Native Modalize', 'React Native Reanimated'],
+    libs: ['Flash List', 'React Native Modalize', 'React Native Reanimated'],
     url: 'https://github.com/Miguelpenha/Testing-Animations/tree/main/pages/AnimatedHomePageOfMovies',
     description: 'Animação de página inicial de filmes, onde há os filmes em cartaz, e é atualizado quando a lista é arrastada para baixo. Quando o filme é clicado, é aberto um modal com informações sobre o filme, como nome, quantidade de votos, média da nota, e um botão de compartilhar e outro de abrir'
 }
--- pages/AnimatedHomePageOfNews/page.ts
+++ pages/AnimatedHomePageOfNews/page.ts
@@ -2,7 +2,7 @@ import { IPage } from '../../types'
 
 const page: IPage = {
     apis: ['TabNews'],
-    libs: ['React Native Reanimated'],
+    libs: ['Flash List', 'React Native Reanimated'],
     title: 'Página inicial animada de notícias',
     url: 'https://github.com/Miguelpenha/Testing-Animations/tree/main/pages/AnimatedHomePageOfNews',
     description: 'Animação de página inicial de notícias, onde há notícias do TabNews, e é atualizado quando a lista é arrastada para baixo. Quando a notícia é clicada ela é aberta, quando a notícia é pressionada é aberto o menu de compartilhar'
